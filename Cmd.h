450347373bf90ba28d1d301c0e55d07d5fb25697
SpringMemory <126363200+bpr1ch3@users.noreply.github.com>
2025-10-19 18:08:18 -0500

    Update Cmd.h

--- Cmd.h
+++ Cmd.h
@@ -1,4 +1,7 @@
+// Cmd.h
+// Last modified by Golden Hour Software on October 19, 2025
 #pragma once
 int secmd();
 fs::path ChangeDirFix(const std::string ascmd);
+
 extern bool IsCustomCommandRun;
